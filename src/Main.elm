bbb6c41cd4c20e53a3dc222aa72670187b88693c
Arttu Moisio <arttu.moisio@tuni.fi>
2021-01-09 15:08:50 +0200

    onkö

--- src/Main.elm
+++ src/Main.elm
@@ -5,61 +5,37 @@ module Main exposing (..)
 -- Read how it works:
 --   https://guide.elm-lang.org/architecture/buttons.html
 --
-
-
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 import String exposing (slice)
 
-
-
 -- MAIN
-
-
+main : Program () Model Msg
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
-
-
 -- MODEL
-
-
 type alias Model = String
-
 
 init : Model
 init =
-  "F"
-
-
+  ""
 
 -- UPDATE
-
-
 type Msg
-  = Increment
-  | Decrement
-
+  = FetchVormi
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Increment ->
-      model ++ "F"
-
-    Decrement ->
-      slice 0 -1 model
-
-
+    FetchVormi ->
+      "No vittu onKÖ"
 
 -- VIEW
-
-
 view : Model -> Html Msg
 view model =
   div []
-    [ button [ onClick Decrement ] [ text "-" ]
+    [ button [ onClick FetchVormi ] [ text "Kato onko Slerba vormissa" ]
     , div [] [ text ( model) ]
-    , button [ onClick Increment ] [ text "+" ]
     ]
